00c4d809bd44288a04cde47feb94710115aa8fdf
USRSIG <USRSIG>
2017-01-20 11:55:05 -0500

    commit con impresion al reves

--- src/epn/Aplicacion.java
+++ src/epn/Aplicacion.java
@@ -6,8 +6,27 @@ public class Aplicacion {
 		ListaEnlazadaDo lista= new ListaEnlazadaDo();
 		lista.insertarInicio("prueba 1");
 		lista.insertarInicio("prueba 2");
+		lista.insertarFinal("prueba 3");
+		lista.insertarInicio("prueba 4");
+		lista.insertarFinal("prueba 5");
+		lista.insertarInicio("0");
 		lista.imprimir();
-
+		lista.imprimirRev();
+		
+		try{
+		System.out.println(lista.buscarInf("prueba 5").getElemento());
+		}catch(NullPointerException e){
+			System.out.println("No se encuentra en la lista");
+		}
+		try{
+		System.out.println(lista.buscarInd(1).getElemento());
+		}catch(NullPointerException ex){
+			System.out.println("La posicion requerida no existe en la lista");
+		}
+		System.out.println("");
+		lista.eliminarDato("prueba 4");
+		lista.imprimir();
+		
 	}
 
 }
--- src/epn/ListaEnlazadaDo.java
+++ src/epn/ListaEnlazadaDo.java
@@ -23,18 +23,89 @@ public class ListaEnlazadaDo {
 	
 	public void insertarInicio(String elem){
 		Nodo actual= new Nodo(elem);
-		actual.setSiguiente(cabeza);
-		
-		if(vacio()!=false){
+		if(vacio()){
 			cabeza=actual;
-			cabeza.setAnterior(actual);
+			//cola=actual;
 			}
+		else{
+			actual.setSiguiente(cabeza);
+			cabeza.setAnterior(actual);
+			cabeza=actual;
+		}
+	}
+	
+	public void insertarFinal(String elem){
+		Nodo actual= new Nodo(elem);
+		if(vacio()){
+			cabeza=actual;
+			cola=actual;
+		}
+		else{
+			Nodo p=cabeza;
+			while(p.getSiguiente()!=null){
+				p=p.getSiguiente();
+			}
+			p.setSiguiente(actual);
+			actual.setAnterior(p);
+			cola=actual;
+		}
+	}
+	
+	public Nodo buscarInf(String dato){
+		Nodo buscador;
+		for(buscador=cabeza;buscador!=null;buscador=buscador.getSiguiente()){
+			if(dato==buscador.getElemento()){
+				return buscador;
+			}
+		}return null;
+	}
+	
+	public Nodo buscarInd(int ref){
+		if(cabeza!=null){
+			Nodo buscador= cabeza;
+			int ind=1;
+			for(buscador=cabeza;(buscador!=null)&&(ind<=ref);ind++){
+				if(ind==ref){
+					return buscador;
+				}
+				buscador=buscador.getSiguiente();
+			}
+				
+			}return null;
+		}
+	
+	public void eliminarDato(String elem){
+		Nodo actual, anterior;
+		boolean comprobador;
+	
+		actual=cabeza;
+		anterior=null;
+		comprobador=false;
+		
+		while ((actual!=null) && (comprobador==false)){
+			comprobador=(actual.getElemento()==elem);
+		
+			if (comprobador==false){
+				anterior=actual;
+				actual=actual.getSiguiente();
+			}
+		}
+		
+		if (actual!=null){
+			if (actual==cabeza){
+				cabeza=actual.getSiguiente();
+			}
+			else{
+				anterior.setSiguiente(actual.getSiguiente());
+			}
+		actual=null;
+		}
+		
 	}
 	
 	public void imprimir(){
 		if(vacio()){
 			System.out.println("Vacia \n");
-			return;
 		}
 		
 		Nodo actual=cabeza;
@@ -42,12 +113,18 @@ public class ListaEnlazadaDo {
 			System.out.println(actual.getElemento());
 			actual=actual.getSiguiente();
 		}
-		System.out.println("\n");
+		System.out.println("");
 	}
 	
-	
-	
-	
-	
-
+	public void imprimirRev(){
+		if(vacio()){
+			System.out.println("Vacia \n");
+	}
+		Nodo actual=cola;
+		while(actual!=null){
+			System.out.println(actual.getElemento());
+			actual=actual.getAnterior();
+		}
+		System.out.println("");
+	}
 }
